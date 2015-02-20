04939c7b8bd23e1b7f16de72668b023c87e44b38
Mark Kirby <mkirby111@gmail.com>
2015-02-19 22:46:20 -0800

    It works!
    I made some tweaks and got the mergeSort function to work.

--- MergeSort.java
+++ MergeSort.java
@@ -8,7 +8,7 @@ public class MergeSort {
 	//mergeSort: sorts input array by recursive mergesort
 	//Input: An array A[0…n-1] orderable integers
 	//Output: Array A[0…n-1] sorted in nondecreasing order
-	public int[] MergeSort(int[] inputArray) {
+	public int[] mergeSort(int[] inputArray) {
 		if (inputArray.length > 1) {
 			//Get lengths for sub-arrays
 			int lengthArray1 = inputArray.length/2;
@@ -20,11 +20,11 @@ public class MergeSort {
 			for (int i = 0; i < lengthArray1; i++) 
 				array1[i] = inputArray[i];
 			for (int j = lengthArray1; j < inputArray.length; j++)
-				array2[j] = inputArray[j];
+				array2[j - lengthArray1] = inputArray[j];
 
 			//Recursively call mergeSort on sub-arrays
-			MergeSort(array1);
-			MergeSort(array2);
+			mergeSort(array1);
+			mergeSort(array2);
 
 			//Merge the 2 sorted sub-arrays into one sorted array
 			int i = 0, j = 0, k = 0;
@@ -39,25 +39,34 @@ public class MergeSort {
 				k++;
 			}
 			//If one sub-array is completely copied, copy the remaining elements from the other sub-array
-			if (i == lengthArray1) {
-				while (j < lengthArray2) {
-					inputArray[k] = array2[j];
-					j++;
-					k++;
-				}
-			} else {
-				while (i < lengthArray1) {
-					inputArray[k] = array1[j];
-					i++;
-					k++;
-				}
+			while (j != lengthArray2) {
+				inputArray[k] = array2[j];
+				j++;
+				k++;
+			}
+			while (i != lengthArray1) {
+				inputArray[k] = array1[i];
+				i++;
+				k++;
 			}
 		}
 		return inputArray; //return sorted input array
 	}
-	
+
 	public static void main(String args[]) { 
-		System.out.println("Should print here");
-		
+		MergeSort m = new MergeSort();
+
+		int[] unsorted = RandomArray.generateRandom();
+		System.out.print("Unsorted array: ");
+		for (int i = 0; i < unsorted.length; i++)
+			System.out.print(unsorted[i] + " ");
+		System.out.println();
+
+		int[] sorted = m.mergeSort(unsorted);
+		System.out.print("Sorted array:   ");
+		for (int i = 0; i < sorted.length; i++)
+			System.out.print(sorted[i] + " ");
+		System.out.println();
+
 	}
 }
--- RandomArray.java
+++ RandomArray.java
@@ -12,12 +12,13 @@ class RandomArray {
   public static final int ARRAY_SIZE = 10;
   public static final int RANINT_MAX = 50;
 
-  public static void generateRandom() {
+  public static int[] generateRandom() {
     int[] data = new int[ARRAY_SIZE];
     Random random = new Random();
     
     for (int i = 0; i < ARRAY_SIZE; ++i) {
       data[i] = random.nextInt(RANINT_MAX);   
     }
+	return data;
   }
 }
