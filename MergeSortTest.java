58bd97adcb378c3cc7e02a8816c88882bbace76c
Sasha Fahrenkopf <sfahrenk@gmail.com>
2015-02-20 20:46:34 -0800

    :q

--- MergeSortTest.java
+++ MergeSortTest.java
@@ -1,22 +1,18 @@
 import static org.junit.Assert.*;
-
+import java.util.Arrays;
 import java.util.Random;
-
 import org.junit.Test;
 
 public class MergeSortTest {
-
+	
 	@Test
 	public void testIsSorted() {
-		  final int ARRAY_SIZE = 10;
-		  final int RANINT_MAX = 50;
-		  
-		  int[] data = new int[ARRAY_SIZE];
-		  Random random = new Random();
-		    
-		  for (int i = 0; i < ARRAY_SIZE; ++i) {
-		    data[i] = random.nextInt(RANINT_MAX);   
-		  }
-		fail("Not yet implemented");
+		MergeSort mergeSort = new MergeSort();
+		int[] unsorted = RandomArray.generateRandom();
+		int[] mergeOutput = new int[10];
+		
+		mergeOutput = mergeSort.mergeSort(unsorted);
+		Arrays.sort(unsorted);
+		assertArrayEquals(unsorted, mergeOutput);
 	}
 }
